06941821bc949268a4ec55a8afe496d865c1662f
bonavadeur <daodaihiep22ussr@gmail.com>
2024-11-25 14:33:08 +0700

    fix patch image

--- hack/replace-image.sh
+++ hack/replace-image.sh
@@ -3,7 +3,7 @@
 TAG=dev
 
 # replace image of queue-proxy
-kubectl -n knative-serving patch image queue-proxy --patch --type=merge \
+kubectl -n knative-serving patch image queue-proxy --type=merge --patch \
     '{"spec":{"image":"docker.io/bonavadeur/nonna:'${TAG}'"}}'
 kubectl -n knative-serving patch configmap config-deployment --patch \
     '{"data":{"queue-sidecar-image":"docker.io/bonavadeur/nonna:'${TAG}'"}}'
